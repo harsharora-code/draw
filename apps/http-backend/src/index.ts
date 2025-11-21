cfc005813148b88578d2fd76845d19376e834e12
Harsh Arora <harshpunyani20@gmail.com>
2025-11-21 21:12:27 +0530

    update-db

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,43 +1,71 @@
 import jwt, { JwtPayload } from "jsonwebtoken"
-import express from 'express'
+import express, { json } from 'express'
 import { JWT_SECRET} from "@repo/backend-common/config";
 import { middleware } from "./middleware.js";
 import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/type"
 import { prismaClient } from "@repo/db/client";
+import cors from "cors"
 
 
 const app = express();
 
+app.use(cors());
 
-app.post('/signup', (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success) {
-        console.log(data.error);
+
+app.post('/signup', async (req, res) => {
+    const parseData = CreateUserSchema.safeParse(req.body);
+    if(!parseData.success) {
+        console.log(parseData.error);
         res.json({
             msg: "Invalid Inpuits"
         })
         return
 
     }
-    res.json({
-        userId : 1232
-    })
+    try {
+        const user = await prismaClient.user.create({
+            data : {
+                email : parseData.data?.email,
+                username: parseData.data.username,
+                password : parseData.data.password
+            }
+        })
+        res.json({
+            userId : user.id
+        })
+    } catch(e) {
+            res.status(411).json({
+            msg: "user already exists"
+        })
+    }
+   
 
 })
 
-app.post('/signin', (req, res) => {
-     const data = SigninSchema.safeParse(req.body);
-    if(!data.success) {
-        console.log(data.error);
-        res.json({
+app.post('/signin', async (req, res) => {
+     const parseData = SigninSchema.safeParse(req.body);
+    if(!parseData.success) {
+        console.log(parseData.error);
+       return res.json({
             msg: "Invalid Inpuits"
         })
-        return
+       
 
     }
-    const userId = 1;
+    const {email, password} = parseData.data;
+    const user = await prismaClient.user.findFirst({
+        where : {
+          email, password
+        }
+    })
+    if(!user) {
+        return res.status(411).json({
+            msg: "unauthorized"
+        })
+    }
+ 
     const encode = jwt.sign({
-        userId,
+        userId: user?.id,
     }, JWT_SECRET as string)
     res.json({
         token : encode
