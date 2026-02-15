512f6339187b6f967d118fe032bc3859aa3b6f0a
Harsh Arora <harshpunyani20@gmail.com>
2026-02-15 15:34:28 +0530

    fix_scripts

---
+++ packages/db/prisma/migrations/20260215095923_fix_schema/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `message` to the `Chat` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Chat" ADD COLUMN     "message" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "photo" TEXT;
