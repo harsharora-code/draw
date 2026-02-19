9154ec43ef54227f20a4a6ba6f0473aa7890a855
Harsh Arora <harshpunyani20@gmail.com>
2026-02-19 11:07:04 +0530

    fix_ws_server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,7 @@
 
 import { WebSocketServer, WebSocket } from 'ws';
 import { URLSearchParams } from 'url';
-import  jwt, { JwtPayload } from 'jsonwebtoken'
+import  jwt from 'jsonwebtoken'
 import {JWT_SECRET} from "@repo/backend-common/config"
 
 // const server = createServer({
@@ -16,23 +16,24 @@ interface User {
   userId: string
 }
 const users: User[] = [];
-function checkUser(token: string): string | null { 
+function checkUser(token: string): string | null {
   try {
+    const decoded = jwt.verify(token, JWT_SECRET);
 
-    const decode = jwt.verify(token, JWT_SECRET);
-    if(typeof decode == "string") {
-      return null
+    if (typeof decoded == "string") {
+      return null;
     }
-    if(!decode || !decode.userId) {
-      return null
-    }
-    return decode.userId;
 
+    if (!decoded || !decoded.userId) {
+      return null;
+    }
+
+    return decoded.userId;
   } catch(e) {
     return null;
   }
-} 
-
+  return null;
+}
 wss.on('connection', function connection(ws, request) {
   const url = request.url;
   if(!url) {
@@ -41,6 +42,11 @@ wss.on('connection', function connection(ws, request) {
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token') || "";
   const userId = checkUser(token);
+
+  console.log("request url: ", request.url);
+  console.log("token: ", token);
+  console.log("userId: ", userId)
+  console.log(JWT_SECRET)
   
   if(userId == null) {
     ws.close();
